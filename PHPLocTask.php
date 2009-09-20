510ee6b0d6bb5417523a598e5666685e7049ca21
Raphael Stolt <stolt@poseidon.(none)>
2009-09-21 00:49:29 +0200

    Adopted task to play along with PHPLoc 1.2.0

--- PHPLocTask.php
+++ PHPLocTask.php
@@ -2,8 +2,8 @@
 require_once 'phing/Task.php';
 require_once 'phing/BuildException.php';
 require_once 'PHPLOC/Analyser.php';
-require_once 'PHPLOC/Util/FilterIterator.php';
-require_once 'PHPLOC/TextUI/ResultPrinter.php';
+require_once 'PHPLOC/TextUI/ResultPrinter/Text.php';
+require_once 'PHPLOC/TextUI/ResultPrinter/Xml.php';
 
 class PHPLocTask extends Task
 {
@@ -11,6 +11,7 @@ class PHPLocTask extends Task
     protected $acceptedReportTypes = null;
     protected $reportDirectory = null;
     protected $reportType = null;
+    protected $countTests = null;
     protected $fileToCheck = null;
     protected $filesToCheck = null;
     protected $reportFileName = null;
@@ -23,6 +24,7 @@ class PHPLocTask extends Task
         $this->reportFileName = 'phploc-report';
         $this->fileSets = array();
         $this->filesToCheck = array();
+        $this->countTests = false;
     }
     public function setSuffixes($suffixListOrSingleSuffix) {
         if (stripos($suffixListOrSingleSuffix, ',')) {
@@ -35,6 +37,11 @@ class PHPLocTask extends Task
     public function setFile(PhingFile $file) {
         $this->fileToCheck = trim($file);
     }
+
+    public function setCountTests($countTests) {
+        $this->countTests = (bool) $countTests;
+    }
+
     public function createFileSet() {
         $num = array_push($this->fileSets, new FileSet());
         return $this->fileSets[$num - 1];
@@ -48,7 +55,36 @@ class PHPLocTask extends Task
     public function setReportDirectory($directory) {
         $this->reportDirectory = trim($directory);
     }
-    public function main() {       
+    public function main() {
+        $this->_validateProperties();
+        if (!is_null($this->reportDirectory) && !is_dir($this->reportDirectory)) {
+            $reportOutputDir = new PhingFile($this->reportDirectory);
+            $logMessage = "Report output directory doesn't exist, creating: " 
+                . $reportOutputDir->getAbsolutePath() . '.';
+            $this->log($logMessage);
+            $reportOutputDir->mkdirs();
+        }
+        if ($this->reportType !== 'cli') {
+            $this->reportFileName.= '.' . trim($this->reportType);
+        }
+        if (count($this->fileSets) > 0) {
+            $project = $this->getProject();
+            foreach ($this->fileSets as $fileSet) {
+                $directoryScanner = $fileSet->getDirectoryScanner($project);
+                $files = $directoryScanner->getIncludedFiles();
+                $directory = $fileSet->getDir($this->project)->getPath();
+                foreach ($files as $file) {
+                    if ($this->isFileSuffixSet($file)) {
+                        $this->filesToCheck[] = $directory . DIRECTORY_SEPARATOR 
+                            . $file;
+                    }
+                }
+            }
+            $this->filesToCheck = array_unique($this->filesToCheck);
+        }
+        $this->runPhpLocCheck();
+    }
+    private function _validateProperties() {
         if (!isset($this->fileToCheck) && count($this->fileSets) === 0) {
             $exceptionMessage = "Missing either a nested fileset or the "
                 . "attribute 'file' set.";
@@ -75,30 +111,9 @@ class PHPLocTask extends Task
                 . "attribute; not both.";
             throw new BuildException($exceptionMessage);
         }
-        if (!is_null($this->reportDirectory) && !is_dir($this->reportDirectory)) {
-            $reportOutputDir = new PhingFile($this->reportDirectory);
-            $logMessage = "Report output directory does't exist, creating: " 
-                . $reportOutputDir->getAbsolutePath() . '.';
-            $this->log($logMessage);
-            $reportOutputDir->mkdirs();
-        }
-        if ($this->reportType !== 'cli') {
-            $this->reportFileName.= '.' . trim($this->reportType);
-        }
-        if (count($this->fileSets) > 0) {
-            $project = $this->getProject();
-            foreach ($this->fileSets as $fileSet) {
-                $directoryScanner = $fileSet->getDirectoryScanner($project);
-                $files = $directoryScanner->getIncludedFiles();
-                $directory = $fileSet->getDir($this->project)->getPath();
-                foreach ($files as $file) {
-                    if ($this->isFileSuffixSet($file)) {
-                        $this->filesToCheck[] = $directory . DIRECTORY_SEPARATOR 
-                            . $file;
-                    }
-                }
-            }
-            $this->filesToCheck = array_unique($this->filesToCheck);
+        if (!is_bool($this->countTests)) {
+            $exceptionMessage = "'countTests' attribute has no boolean value";
+            throw new BuildException($exceptionMessage);
         }
         if (!is_null($this->fileToCheck)) {
             if (!$this->isFileSuffixSet($file)) {
@@ -107,7 +122,6 @@ class PHPLocTask extends Task
                 throw new BuildException($exceptionMessage);
             }
         }
-        $this->runPhpLocCheck();
     }
     protected function isFileSuffixSet($filename) {
         $pathinfo = pathinfo($filename);
@@ -117,9 +131,9 @@ class PHPLocTask extends Task
     protected function runPhpLocCheck() {
         $files = $this->getFilesToCheck();
         $result = $this->getCountForFiles($files); 
-        
+
         if ($this->reportType === 'cli' || $this->reportType === 'txt') {
-            $printer = new PHPLOC_TextUI_ResultPrinter;
+            $printer = new PHPLOC_TextUI_ResultPrinter_Text();
             if ($this->reportType === 'txt') {
                 ob_start();
                 $printer->printResult($result);
@@ -136,13 +150,13 @@ class PHPLocTask extends Task
                 $printer->printResult($result);
             }
         } elseif ($this->reportType === 'xml') {
-            $xml = $this->getResultAsXml($result);
+            $printer = new PHPLOC_TextUI_ResultPrinter_XML();
             $reportDir = new PhingFile($this->reportDirectory);
             $logMessage = "Writing report to: " . $reportDir->getAbsolutePath()
                 . DIRECTORY_SEPARATOR . $this->reportFileName;
             $this->log($logMessage);
-            file_put_contents($this->reportDirectory . DIRECTORY_SEPARATOR
-                . $this->reportFileName, $xml);
+            $printer->printResult($this->reportDirectory . DIRECTORY_SEPARATOR
+                . $this->reportFileName, $result);
         }
     }
     protected function getFilesToCheck() {
@@ -157,45 +171,7 @@ class PHPLocTask extends Task
         return $files;
     }
     protected function getCountForFiles($files) {
-        $count = array('files' => 0, 'loc' => 0, 'cloc' => 0, 'ncloc' => 0,
-            'eloc' => 0, 'interfaces' => 0, 'classes' => 0, 'functions' => 0);
-        $directories = array();
-
-        foreach ($files as $file) {
-            $directory = $file->getPath();
-            if (!isset($directories[$directory])) {
-                $directories[$directory] = TRUE;
-            }          
-            PHPLOC_Analyser::countFile($file->getPathName(), $count);
-        }
-        
-        if (!function_exists('parsekit_compile_file')) {
-            unset($count['eloc']);
-        }
-        $count['directories'] = count($directories) - 1;
-        return $count;
-    }
-    protected function getResultAsXml($result) {        
-        $newline = "\n";
-        $newlineWithSpaces = sprintf("\n%4s",'');
-        $xml = '<?xml version="1.0" encoding="UTF-8"?>';
-        $xml.= $newline . '<phploc>'; 
-        
-        if ($result['directories'] > 0) {
-            $xml.= $newlineWithSpaces . '<directories>' . $result['directories'] . '</directories>';
-            $xml.= $newlineWithSpaces . '<files>' . $result['files'] . '</files>';
-        }
-        $xml.= $newlineWithSpaces . '<loc>' . $result['loc'] . '</loc>';
-        
-        if (isset($result['eloc'])) {
-            $xml.= $newlineWithSpaces . '<eloc>' . $result['eloc'] . '</eloc>';
-        }
-        $xml.= $newlineWithSpaces . '<cloc>' . $result['cloc'] . '</cloc>';
-        $xml.= $newlineWithSpaces . '<ncloc>' . $result['ncloc'] . '</ncloc>';
-        $xml.= $newlineWithSpaces . '<interfaces>' . $result['interfaces'] . '</interfaces>';
-        $xml.= $newlineWithSpaces . '<classes>' . $result['classes'] . '</classes>';
-        $xml.= $newlineWithSpaces . '<methods>' . $result['functions'] . '</methods>' . $newline;
-        $xml.= '</phploc>';
-        return $xml;
+        $analyser = new PHPLoc_Analyser(); 
+        return $analyser->countFiles($files, $this->countTests);
     }
 }
